690a90bf7f1cf737b97bb10c6cf6c66eead89c5e
Elton Miguel Cachiliva Chimbile <71854893+ecachiliva@users.noreply.github.com>
2025-05-15 21:16:10 +0000

    Actaulizacion hora:22:17 dia: 15/05/2025

--- portfolio/src/components/Contact/contact.js
+++ portfolio/src/components/Contact/contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
 import Walmart from '../../assets/walmart.png';
 import Adobe from '../../assets/adobe.png';
@@ -9,9 +9,26 @@ import FacebookIcon from '../../assets/facebook-icon.png';
 import TwitterIcon from '../../assets/twitter.png';
 import YouTubeIcon from '../../assets/youtube.png';
 import InstagramIcon from '../../assets/instagram.png';
+import emailjs from '@emailjs/browser';
 
 
-const Contact = () => {
+    export const Contact = () => {
+        const form = useRef();
+        const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_exe8b6o', 'template_a5t8gar', form.current, {publicKey: 'qECRMHltOEqR5OnLv',})
+            .then(() => {
+                console.log('SUCCESS!');
+                e.target.reset();
+                alert('Email Sent !')
+                    }, (error) => {
+                        console.log('FAILED...', error.text);
+                    },
+                );
+    };
+
+
   return (
     <section id="contactPage">
         <div id="clients">
@@ -30,11 +47,11 @@ const Contact = () => {
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc"> Please fill out the form below to discuss any work opportunities</span>
-            <form className="contactForm">
-                <input type="text" className="name" placeholder="Your Name" />
-                <input type="text" className="email" placeholder="Your Name" />
-                <textarea className="msg" name="message" rws="8" placeholder="Your Message"></textarea>
-                <button type='submit' value='send' className="submitBtn">Submit</button>
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                <input type="text" className="name" placeholder="Your Name" name='your_name' />
+                <input type="email" className="email" placeholder="Your Email" name='your_email' />
+                <textarea name="message" placeholder='Your Message' rows={5} className='msg' ></textarea>
+                <button type="submit" value="Send" className='submitBtn'>Submit</button>
                 <div className="links">
                     <img src= {FacebookIcon} alt="Facebook" className="link" />
                     <img src= {TwitterIcon} alt="Twitter" className="link" />
@@ -45,6 +62,6 @@ const Contact = () => {
         </div>
     </section>
   );
-}
+} 
 
 export default Contact;
